e3399151292dedfefcb78d669d47b5dd11852b79
okba bekhelifi <okba.bekhelifi@gmail.com>
2019-09-29 11:27:15 +0200

    - Added base file for process

--- aawedha/analysis/preprocess.py
+++ aawedha/analysis/preprocess.py
@@ -3,5 +3,13 @@ import numpy as np
 
 
 def bandpass(eeg, band, fs, order=2):
+  """
+  """
   B,A = sig.butter(order, np.array(band)/(fs/2), btype='bandpass')
   return sig.filtfilt(B, A, eeg, axis=0)
+
+
+def fit_normalize():
+  """
+  """
+  NotImplementedError
