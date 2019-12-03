277ed08c926914d64f50bd4dd844198146eff543
Okba Bekhelifi <okba.bekhelifi@gmail.com>
2019-12-03 22:33:49 +0100

    - issue #6 : 	- modfied PodNet  On branch dev  Changes to be committed: 	modified:   models/durham.py

--- aawedha/models/durham.py
+++ aawedha/models/durham.py
@@ -20,8 +20,9 @@ def D1SCU(Samples=256, nb_classes=5):
     '''
     input1 = Input(shape=(1, Samples))
     #
-    block1 = Conv1D(16, kernel_size= 10, padding='same', kernel_regularizer=l2(0.001),
-                strides=4,input_shape=(1, Samples), use_bias=False)(input1)
+    block1 = Conv1D(16, kernel_size=10, padding='same',
+                    kernel_regularizer=l2(0.001), strides=4,
+                    input_shape=(1, Samples), use_bias=False)(input1)
     block1 = BatchNormalization(axis=1)(block1)
     block1 = Activation('relu',  activity_regularizer=l2(0.001))(block1)
     block1 = MaxPooling1D(pool_size=2, padding='same')(block1)
@@ -50,36 +51,40 @@ def PodNet(nb_classes, Chans=64, Samples=128,
     '''
     input1 = Input(shape=(1, Chans, Samples))
     #
-    block1 = Conv2D(Chans, (1, 30), padding='same',
+    block1 = Conv2D(100, (10, 30), padding='same',
                     input_shape=(1, Chans, Samples),
                     use_bias=False)(input1)
     block1 = Dropout(dropoutRate)(block1)
     block1 = BatchNormalization(axis=1)(block1)
     block1 = Activation('relu')(block1)
-    block1 = MaxPooling1D(pool_size=(2,2), strides=2, padding='same')(block1)
+    block1 = MaxPooling2D(pool_size=(2, 2), strides=2, padding='same')(block1)
     #
-    block2 = Conv1D(1, kernel_size= 30, padding='same',  strides=4, use_bias=False)(block1)
+    block2 = Conv1D(100, kernel_size=30, padding='same',  strides=4,
+                    use_bias=False)(block1)
     block2 = Dropout(dropoutRate)(block2)
     block2 = BatchNormalization(axis=1)(block2)
     block2 = Activation('relu')(block2)
-    block2 = MaxPooling1D(pool_size=(2,2), strides=2, padding='same')(block2)
+    block2 = MaxPooling1D(pool_size=(2, 2), strides=2, padding='same')(block2)
     #
-    block3 = Conv1D(1, kernel_size= 30, padding='same',  strides=4, use_bias=False)(block2).
+    block3 = Conv1D(100, kernel_size=30, padding='same',  strides=4,
+                    use_bias=False)(block2)
     block3 = Dropout(dropoutRate)(block3)
     block3 = BatchNormalization(axis=1)(block3)
     block3 = Activation('relu')(block3)
-    block3 = MaxPooling1D(pool_size=(2,2), strides=2, padding='same')(block3)
+    block3 = MaxPooling1D(pool_size=(2, 2), strides=2, padding='same')(block3)
     #
-    block4 = Conv1D(1, kernel_size= 30, padding='same',  strides=4, use_bias=False)(block3)
+    block4 = Conv1D(100, kernel_size=30, padding='same',  strides=4,
+                    use_bias=False)(block3)
     block4 = Dropout(dropoutRate)(block4)
     block4 = BatchNormalization(axis=1)(block4)
     block4 = Activation('relu')(block4)
-    block4 = MaxPooling1D(pool_size=(2,2), strides=2, padding='same')(block4)
+    block4 = MaxPooling1D(pool_size=(2, 2), strides=2, padding='same')(block4)
     #
-    block5 = Conv1D(1, kernel_size= 30, padding='same',  strides=4, use_bias=False)(block4)
+    block5 = Conv1D(100, kernel_size=30, padding='same',  strides=4,
+                    use_bias=False)(block4)
     block5 = BatchNormalization(axis=1)(block5)
     block5 = Activation('relu')(block5)
-    block5 = MaxPooling1D(pool_size=(2,2), strides=2, padding='same')(block5)
+    block5 = MaxPooling1D(pool_size=(2, 2), strides=2, padding='same')(block5)
     dense = Dense(nb_classes)(block5)
     #
     softmax = Activation('softmax')(dense)
