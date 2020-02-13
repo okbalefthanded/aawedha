53dbf4e628607081b85b37b5c4c498ff627e5592
okba bekhelifi <okba.bekhelifi@gmail.com>
2020-02-13 12:47:43 +0100

    - issue #3 : 	- finished plot_topomaps, using MNE.  On branch dev  Changes to be committed: 	modified:   aawedha/visualization/explain.py

--- aawedha/visualization/explain.py
+++ aawedha/visualization/explain.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-import matplotlib.tri as tri
+import mne
 import numpy as np
 
 
@@ -29,42 +29,18 @@ def plot_temporal_filters(filters, fs):
     plt.show()
 
 
-def pol2cart(rho, phi):
-    x = rho * np.cos(phi)
-    y = rho * np.sin(phi)
-    return(x, y)
-
-def plot_topomaps(data=None, channels=None):
+def plot_topomaps(data=None, channels=None, fs=512):
     '''
     '''
-    fpath = '/content/drive/My Drive/data/ssvep_benchmark_dataset/64-channels.txt'
-    f = open(fpath, 'r')
-    locs = f.read()
-    a = locs.split('\n')
-    pol = [np.array(i.split('\t')[1:3]).astype(float) for i in a if i]
-    pol = np.array(pol)
-    chs = [i.split('\t')[-1].strip() for i in a if i]
-    pol[:,0] = pol[:,0] * (np.pi/180) 
-    x,y = pol2cart(pol[:,1], pol[:,0])  
-    #
-    interp_detail = 100
-    interpx = np.linspace(x.min()-.2, x.max()+.25, interp_detail)
-    interpy = np.linspace(y.min(), y.max(), interp_detail)
-
-    gridx, gridy = np.meshgrid(interpx, interpy)
-
-    dat = np.random.uniform(-2,2,64)
-    triang = tri.Triangulation(y, x)
-    interpolator = tri.LinearTriInterpolator(triang, dat)
-    zi = interpolator(gridx, gridy)
-
-    an = np.linspace(0, 2 * np.pi, 100)
-    plt.plot(.8*np.cos(an), .8*np.sin(an),color='black')
-    plt.scatter(y,x,marker='o',c='black')
-    plt.contourf(interpy, interpx, zi, cmap="RdBu_r", alpha=.5)
-    #plt.xlim([-.6, .6])
-    #plt.ylim([-.6,.6])
-    plt.title('Electrode locations')
-    plt.colorbar()
-
-    plt.show() 
+    montage = mne.channels.make_standard_montage('standard_1020')
+    info = mne.create_info(channels, fs, 'eeg', montage=montage)
+    if data.ndim == 2:
+      fig, ax = plt.subplots(1, data.shape[1])
+      #
+      for i in range(data.shape[1]):
+        mne.viz.plot_topomap(data[:,i],
+                     pos=info, axes=ax[i], show=False)
+        ax[i].set(title=f'Spatial Filter {i}')
+    else:      
+      mne.viz.plot_topomap(data, pos=info, show=False)
+    
