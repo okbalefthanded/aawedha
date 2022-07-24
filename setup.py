08f429a88d59f0323e669cc4b603b0f1f1cb5c74
Okba BEKHELIFI <okba.bekhelifi@gmail.com>
2022-07-24 19:03:01 +0100

    - Updated setup.py  Changes to be committed: 	modified:   setup.py
    	modified:   aawedha/models/pytorch/few_filtersPTH.py
    	modified:   aawedha/visualization/signal.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
 	long_description_content_type="text/markdown",	
 	packages=setuptools.find_packages(),
 	classifiers=[
-		"Programming Language :: Python :: 3.6",
+		"Programming Language :: Python :: 3.7",
 		"Licence :: OSI Approved :: GNU General Public License v3.0",
 		"Operating System :: OS Independent",
 	]
