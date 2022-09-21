- splitted spelling rate calculation for fixed/flexible number of   flashes  On branch dev  Changes to be committed: 	modified:   aawedha/evaluation/evaluation_utils.py 	modified:   aawedha/io/epfl.py 	modified:   aawedha/io/essex.py 	modified:   aawedha/io/freiburg_online.py 	modified:   aawedha/paradigms/utils_paradigms.py

aawedha/io/epfl.py
@@ -37,7 +37,7 @@ class EPFL(DataSet):
         self.test_events = []
         self.phrase = []
         self.test_phrase = []
-        self.flashes_test = []
+        self.test_flashes = []
 
     def generate_set(self, load_path=None,
                      download=False,
@@ -125,7 +125,7 @@ class EPFL(DataSet):
         Y_test = []
         ph = []
         ph_test = []
-        flashes_test = []
+        test_flashes = []
         for sbj in range(1, subjects + 1):
             target = []
             if sbj == 5:
@@ -158,7 +158,7 @@ class EPFL(DataSet):
             Y_test.append(test_y.astype(np.float32))
             events.append(np.concatenate(stims, axis=-1))
             events_test.append(stm)
-            flashes_test.append(flashes)
+            test_flashes.append(flashes)
             
         #
         self.events = events
@@ -166,7 +166,7 @@ class EPFL(DataSet):
         self.phrase = np.array(ph)
         # self.phrase_test = np.array(ph_test)
         self.test_phrase = np.array(ph_test)
-        self.flashes_test = np.array(flashes_test)
+        self.test_flashes = np.array(test_flashes)
         return X, Y, X_test, Y_test
 
     def download_raw(self, store_path=None):

aawedha/io/freiburg_online.py
@@ -95,9 +95,8 @@ class FreiburgOnline(DataSet):
             del data
 
         samples, channels, trials = X[0].shape
-        X = np.array(X).reshape(
-            (n_subjects, samples, channels, trials), order='F', dtype=np.float32)
-        Y = np.array(Y).reshape((n_subjects, trials), order='F', dtype=np.float32) 
+        X = np.array(X).reshape((n_subjects, samples, channels, trials), order='F').astype(np.float32)
+        Y = np.array(Y).reshape((n_subjects, trials), order='F').astype(np.float32) 
 
         return X, Y
 

aawedha/paradigms/utils_paradigms.py
@@ -1,5 +1,6 @@
 from pyLpov.utils.utils import select_target
 from sklearn.metrics import accuracy_score
+import numpy as np
 
 
 def select_decision(scores, events, paradigm):
@@ -39,18 +40,63 @@ def spelling_rate(preds, op, dataset):
         else:
             phrase = dataset.paradigm.phrase[0]
     events = dataset.test_events[op] 
+    # n_char = len(phrase)
+    if hasattr(dataset, 'test_flashes'):
+        flashes = dataset.test_flashes[op]
+        decision = decision_flexible_trials(preds, dataset, events, phrase, flashes) 
+    else:
+        decision = decision_fixed_trials(preds, dataset, events, phrase)
+    
+    return accuracy_score(phrase, decision)*100
+
+
+def decision_fixed_trials(preds, dataset, events, phrase):
     n_char = len(phrase)
     trials = len(preds) // n_char
     iterations = range(0, len(preds), trials)
     decision = []
     counter = 0
+    stimuli = dataset.paradigm.stimuli
     for j in iterations:
         idx = range(j, j+trials)
         if preds.shape[1] > 1:
-            scores = preds[idx, 1]
+            scores = preds[:, 1]
         else:
-            scores = preds[idx]
-        events_per_char = events[idx]
+            scores = preds.squeeze()
+        
+        if len(idx) > stimuli: # single trial
+            scores = scores[j:j+stimuli]
+            events_per_char = events[j:j+stimuli]
+        else:
+            scores = scores[idx]
+            events_per_char = events[idx]
+        # events_per_char = events[idx]
         decision.append(select_decision(scores, events_per_char, dataset.paradigm))
         counter += 1
-    return accuracy_score(phrase, decision)*100
+    
+    return decision
+
+
+def decision_flexible_trials(preds, dataset, events, phrase, flashes):
+    n_char = len(phrase)
+    trials = flashes // n_char
+    decision = []
+    k = 0
+    stimuli = dataset.paradigm.stimuli
+    for tr in np.nditer(trials):
+        step = (stimuli * tr) + k
+        args = np.arange(k, step)
+        if preds.shape[1] > 1:
+            scores = preds[:, 1]
+        else:
+            scores = preds.squeeze()
+
+        if tr > stimuli: # single trial
+            scores = scores[args[0]:args[0]+stimuli]
+            events_per_char = events[args[0]:args[0]+stimuli]
+        else:
+            scores = scores[args]
+            events_per_char = events[args]
+        decision.append(select_decision(scores, events_per_char, dataset.paradigm))
+        k = step
+    return decision