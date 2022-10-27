520df28b5def70b3ecb77521b4efd816fd79b55d
Okba BEKHELIFI <okba.bekhelifi@gmail.com>
2022-10-27 23:37:06 +0100

    - Added SghirNet19 (MultiBranch big model).  On branch dev  Your branch is up to date with 'origin/dev'.
     Changes to be committed:
    	modified:   aawedha/models/pytorch/sghirnet.py

--- aawedha/models/pytorch/sghirnet.py
+++ aawedha/models/pytorch/sghirnet.py
@@ -1022,3 +1022,99 @@ class SghirNet18(TorchModel):
         x = flatten(x, 1)
         x = self.dense(x)
         return x
+
+# MultiBranch SghirNet10
+def conv_block(in_channels, filters=8, kernel=(1, 64), pad="valid",
+               temp=1024, activation=nn.GELU, dropout_rate=0.5):    
+    return nn.Sequential(
+                Conv2dWithConstraint(in_channels, filters, kernel, padding=pad, 
+                                    groups=1, bias=False, max_norm=1),
+                nn.LayerNorm([filters, 1, temp]),
+                activation(),
+                nn.Dropout(p=dropout_rate)
+                )
+    
+def stem_block(in_channels, filters=8, kernel=(1, 64)):    
+    return nn.Sequential(
+                nn.Conv2d(in_channels, filters, kernel, padding="same", bias=False),
+                nn.BatchNorm2d(filters)
+                )
+
+class SghirNet19(TorchModel):
+
+    def __init__(self, nb_classes=4, Chans=64, Samples=256, kernLength=256,
+                F1=32, F2=16, D=1, dropoutRate=0.5, device="cuda", 
+                name="SghirNet19"):
+        super().__init__(device, name)    
+        expansion = F2*3
+        # like a stem
+        self.conv01 = stem_block(1, F1, (1, kernLength // 4))
+        self.conv02 = stem_block(1, F1, (1, kernLength // 2))
+        self.conv03 = stem_block(1, F1, (1, kernLength))
+        # block1  
+        self.conv11 = conv_block(F1*3, F2, (Chans, 1), dropout_rate=dropoutRate) 
+        self.conv12 = conv_block(F1*3, F2, (Chans, 1), dropout_rate=dropoutRate) 
+        self.conv13 = conv_block(F1*3, F2, (Chans, 1), dropout_rate=dropoutRate)    
+        self.pool1  = BlurPool(expansion, filt_size=(1,2), stride=(1,2))
+        self.skip1  = skip(expansion, expansion, kernLength // 2, kernLength // 4)
+        # block2
+        self.conv21 = conv_block(expansion, F2, (1, kernLength // 2), temp=kernLength // 2, pad='same', dropout_rate=dropoutRate)
+        self.conv22 = conv_block(expansion, F2, (1, kernLength // 4), temp=kernLength // 2, pad='same', dropout_rate=dropoutRate)
+        self.conv23 = conv_block(expansion, F2, (1, kernLength // 8), temp=kernLength // 2, pad='same', dropout_rate=dropoutRate)
+        self.pool2  = BlurPool(expansion, filt_size=(1,2), stride=(1,2))
+        self.skip2  = skip(expansion, expansion, kernLength // 2, kernLength // 4)      
+        # block3
+        self.conv31 = conv_block(expansion, F2, (1, kernLength // 8), temp=kernLength // 4, pad='same', dropout_rate=dropoutRate)
+        self.conv32 = conv_block(expansion, F2, (1, kernLength // 4), temp=kernLength // 4, pad='same', dropout_rate=dropoutRate)
+        self.conv33 = conv_block(expansion, F2, (1, kernLength // 16), temp=kernLength // 4, pad='same', dropout_rate=dropoutRate)
+        self.pool3  = BlurPool(expansion, filt_size=(1,2), stride=(1,2))
+        # block4
+        self.conv41 = conv_block(expansion, F2, (1, kernLength // 16), temp=kernLength // 8, pad='same', dropout_rate=dropoutRate)
+        self.conv42 = conv_block(expansion, F2, (1, kernLength // 32), temp=kernLength // 8, pad='same', dropout_rate=dropoutRate)
+        self.conv43 = conv_block(expansion, F2, (1, kernLength // 64), temp=kernLength // 8, pad='same', dropout_rate=dropoutRate)
+        self.pool4  = BlurPool(expansion, filt_size=(1,2), stride=(1,2))
+        #
+        self.dense = LineardWithConstraint(expansion*2*(Samples // 16), nb_classes, max_norm=0.5)
+
+        # self.initialize_glorot_uniform()
+        initialize_Glorot_uniform(self)
+        
+    def forward(self, x):        
+        x = self._reshape_input(x)
+        
+        x1 = self.conv01(x)
+        x2 = self.conv02(x)
+        x3 = self.conv03(x)
+        x =  torch.cat((x1, x2, x3), dim=1)
+        
+        x1 = self.conv11(x)
+        x2 = self.conv12(x)
+        x3 = self.conv13(x)
+        x =  torch.cat((x1, x2, x3), dim=1)
+        x = self.pool1(x)
+        shortcut1 = x
+        
+        x1 = self.conv21(x)
+        x2 = self.conv22(x)
+        x3 = self.conv23(x)
+        x =  torch.cat((x1, x2, x3), dim=1)
+        x = self.pool2(x)
+        shortcut2 = x
+        x = x + self.skip1(shortcut1)
+
+        x1 = self.conv31(x)
+        x2 = self.conv32(x)
+        x3 = self.conv33(x)
+        x =  torch.cat((x1, x2, x3), dim=1)
+        x = self.pool3(x)
+        x = x + self.skip2(shortcut2)
+
+        x1 = self.conv41(x)
+        x2 = self.conv42(x)
+        x3 = self.conv43(x)
+        x =  torch.cat((x1, x2, x3), dim=1)        
+        
+        x = flatten(x, 1)       
+        x = self.dense(x)
+        
+        return x
