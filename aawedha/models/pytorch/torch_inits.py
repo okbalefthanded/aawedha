20850078571d602a587da18050d4209d7bc8c8c3
Okba <okba.bekhelifi@gmail.com>
2022-11-02 23:28:38 +0100

    - Added LayerNorm to Pytorch inits  On branch dev  Your branch is up to date with 'origin/dev'.
     Changes to be committed:
    	modified:   models/pytorch/torch_inits.py

--- aawedha/models/pytorch/torch_inits.py
+++ aawedha/models/pytorch/torch_inits.py
@@ -9,7 +9,9 @@ def initialize_Glorot_uniform(model):
     """
     for module in model.modules():
         if hasattr(module, 'weight'):
-            if not("BatchNorm" in module.__class__.__name__):
+            # if not("BatchNorm" in module.__class__.__name__):
+            cls_name = module.__class__.__name__            
+            if not("BatchNorm" in cls_name or "LayerNorm" in cls_name):
                 nn.init.xavier_uniform_(module.weight, gain=1)
             else:
                 nn.init.constant_(module.weight, 1)
@@ -26,7 +28,9 @@ def initialize_He_uniform(model):
     """
     for module in model.modules():
         if hasattr(module, 'weight'):
-            if not("BatchNorm" in module.__class__.__name__):
+            # if not("BatchNorm" in module.__class__.__name__):
+            cls_name = module.__class__.__name__            
+            if not("BatchNorm" in cls_name or "LayerNorm" in cls_name):
                 nn.init.kaiming_uniform_(module.weight, mode='fan_out', nonlinearity='relu')
             else: 
                 nn.init.constant_(module.weight, 1)
