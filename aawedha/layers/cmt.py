a8abad899c5dde2d38f9735ad7d80b7f28cbf432
okba bekhelifi <okba.bekhelifi@gmail.com>
2022-11-03 17:08:36 +0100

    - Added CMT attention  On branch dev  Your branch is ahead of 'origin/dev' by 1 commit.    (use "git push" to publish your local commits)
     Changes to be committed:
    	new file:   aawedha/layers/cmt.py

---
+++ aawedha/layers/cmt.py
@@ -0,0 +1,55 @@
+## Taken from official implementation:
+## https://github.com/ggjy/CMT.pytorch
+## Attention Block from 
+## Guo, J. et al. (2022) ‘CMT: Convolutional Neural Networks Meet Vision Transformers’,
+##  pp. 12165–12175. doi: 10.1109/cvpr52688.2022.01186.
+from torch import  nn
+import torch
+import torch.nn.functional as F
+
+
+class Attention(nn.Module):
+    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, 
+                 attn_drop=0., proj_drop=0., qk_ratio=1, sr_ratio=1):
+        super().__init__()
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        self.scale = qk_scale or head_dim ** -0.5
+        self.qk_dim = dim // qk_ratio
+
+        self.q = nn.Linear(dim, self.qk_dim, bias=qkv_bias)
+        self.k = nn.Linear(dim, self.qk_dim, bias=qkv_bias)
+        self.v = nn.Linear(dim, dim, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+        
+        self.sr_ratio = sr_ratio
+        # Exactly same as PVTv1
+        if self.sr_ratio > 1:
+            self.sr = nn.Sequential(
+                nn.Conv2d(dim, dim, kernel_size=sr_ratio, stride=sr_ratio, groups=dim, bias=True),
+                nn.BatchNorm2d(dim, eps=1e-5),
+            )
+
+    def forward(self, x, H, W, relative_pos):
+        B, N, C = x.shape
+        
+        q = self.q(x).reshape(B, N, self.num_heads, self.qk_dim // self.num_heads).permute(0, 2, 1, 3)
+        
+        if self.sr_ratio > 1:
+            x_ = x.permute(0, 2, 1).reshape(B, C, H, W)
+            x_ = self.sr(x_).reshape(B, C, -1).permute(0, 2, 1)
+            k = self.k(x_).reshape(B, -1, self.num_heads, self.qk_dim // self.num_heads).permute(0, 2, 1, 3)
+            v = self.v(x_).reshape(B, -1, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
+        else:
+            k = self.k(x).reshape(B, N, self.num_heads, self.qk_dim // self.num_heads).permute(0, 2, 1, 3)
+            v = self.v(x).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
+        
+        attn = (q @ k.transpose(-2, -1)) * self.scale + relative_pos
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
