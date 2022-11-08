8ba9dd753ce614481806c224e493a347dd657c41
Okba BEKHELIFI <okba.bekhelifi@gmail.com>
2022-11-08 19:51:25 +0100

    minor fixes  On branch dev  Your branch is up to date with 'origin/dev'.
     Changes to be committed:
    	modified:   aawedha/models/pytorch/ccnn.py

--- aawedha/models/pytorch/ccnn.py
+++ aawedha/models/pytorch/ccnn.py
@@ -17,11 +17,15 @@ import torch
 class CCNN(TorchModel):
 
     def __init__(self, nb_classes=4, Chans=8, Samples=220, dropout_rate=0.25, kernLength=10,
-                device='cuda', name='CCNN'):
+                fs=512, resolution=0.293, device='cuda', name='CCNN'):
         super().__init__(device, name)
         filters = 2*Chans
         out_features = (Samples - (kernLength-1) - 1) + 1 
-        
+        self.fs = fs
+        self.resolution = resolution
+        self.nfft  = fs / resolution
+        self.fft_start = 0 
+        self.fft_end = 0
         self.conv1 = nn.Conv2d(Chans, filters, (Chans, 1), padding="valid")
         self.bn1   = nn.BatchNorm2d(filters)
         self.drop1 = nn.Dropout(dropout_rate)
@@ -33,6 +37,7 @@ class CCNN(TorchModel):
         initialize_Glorot_uniform(self)
 
     def forward(self, x):
+        x = self._reshape_input(x)
         x = self.transform(x)
         x = self.drop1(F.relu(self.bn1(self.conv1(x))))
         x = self.drop2(F.relu(self.bn2(self.conv2(x))))
@@ -42,5 +47,9 @@ class CCNN(TorchModel):
 
     def transform(self, x):
         with torch.no_grad():
-            pass
+            samples = x.shape[-1]
+            x = torch.fft.rfft2(x, dim=-1) / samples
+            real = x.real[]
+            imag = x.image[]
+            x = torch.cat((real, imag))
         return x
