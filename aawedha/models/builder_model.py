a6174f5677a1409885e416183e07331fa500f941
okba bekhelifi <okba.bekhelifi@gmail.com>
2023-01-22 16:04:01 +0100

    - Adding SWA  On branch dev  Your branch is up to date with 'origin/dev'.
     Changes to be committed:
    	modified:   aawedha/models/builder_model.py
    	new file:   aawedha/models/pytorch/torch_swa.py

--- aawedha/models/builder_model.py
+++ aawedha/models/builder_model.py
@@ -1,9 +1,9 @@
 from aawedha.models.pytorch.torchmodel import TorchModel
-
+from aawedha.models.pytorch.torch_swa import SWA
 
 train_strategy = {
     'regular': TorchModel,
-    'swa': None,
+    'swa': SWA,
     'twa': None,
     'sam': None,
     'asam': None,
@@ -11,9 +11,14 @@ train_strategy = {
 }
 
 def build_learner(compile_config):
-    strategy = 'regular'
+    
+    ts = TorchModel() # default
     if 'train' in compile_config:
-        strategy = compile_config['train']
+        strategy = compile_config['train']        
         if isinstance(compile_config['train'], list):
-            strategy =  compile_config['train'][0]
-    return train_strategy[strategy]()
+            strategy = compile_config['train'][0]
+            params = compile_config['train'][1]
+            ts = train_strategy[strategy](**params)
+        else:
+            ts = train_strategy[strategy]()            
+    return ts
