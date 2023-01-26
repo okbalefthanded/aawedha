488d0f48c7793f68a6b36d606fc6aef904171c74
Okba BEKHELIFI <okba.bekhelifi@gmail.com>
2023-01-26 15:02:10 +0100

    - Finished adding SAM with new API  On branch dev  Changes to be committed: 	modified:   aawedha/models/pytorch/torch_builders.py

--- aawedha/models/pytorch/torch_builders.py
+++ aawedha/models/pytorch/torch_builders.py
@@ -6,6 +6,7 @@ from aawedha.models.pytorch.samtorch import SAM
 from aawedha.optimizers.adan import Adan
 from libauc.losses import AUCMLoss
 from libauc.optimizers import PESG
+from importlib import import_module
 #from holocron.nn import PolyLoss
 from ranger21 import Ranger21
 from torch import optim
@@ -35,6 +36,9 @@ custom_opt = {
     'Adan': Adan,
     'Ranger': Ranger21,
     'PESG' : PESG,
+}
+
+wrapped_opt = {
     'Sam': SAM
 }
 
@@ -44,7 +48,19 @@ available_callbacks = {
 
 
 def get_optimizer(optimizer, opt_params):
-    """
+    """Create an Optimizer instance from description.
+
+    Parameters
+    ----------
+    optimizer : str | list
+        - optimizer name and parameters
+    opt_params : iterable | dict
+        Pytorch module (model) parameters to optimize.
+
+    Returns
+    -------
+    torch.optim instance
+        optimizer object
     """
     params = {'params': opt_params}
     if isinstance(optimizer, str):
@@ -56,13 +72,72 @@ def get_optimizer(optimizer, opt_params):
         return optimizer   
 
 def _get_optim(opt_id, params):
-    available = list(optim.__dict__.keys())
+    """optimizer creation function
+
+    Parameters
+    ----------
+    opt_id : str
+        optimizer name
+    params : dict
+        optimizer attributes
+
+    Returns
+    -------
+    torch.optim instance
+        optimizer object
+
+    Raises
+    ------
+    ModuleNotFoundError
+        optimizer name is wrong or it is not implemented.
+    """
+    available = list(optim.__dict__.keys())    
     if opt_id in available:
         return getattr(optim, opt_id)(**params)
     elif opt_id in custom_opt:
         return custom_opt[opt_id](**params)
+    elif opt_id in wrapped_opt:
+        return _get_wrapped_optim(opt_id, params)
     else:
         raise ModuleNotFoundError
+
+def _get_wrapped_optim(opt_id, params):
+    """Create a wrapped optimizer.
+    A wrapped optimizer is a torch.optim object that has a base
+    optimizer attribute. eg. SAM. 
+
+    Parameters
+    ----------
+    opt_id : str
+        wrapped optimizer name
+    params : dict 
+        wrapped optimizer params
+    """
+    base_opt = params['base_optimizer']
+    # params['base_optimizer'] = getattr(opt_module(base_opt), base_opt)
+    params['base_optimizer'] = opt_module(base_opt)
+    return wrapped_opt[opt_id](**params)
+
+def opt_module(base_opt):
+    """Get optimizer module
+
+    Parameters
+    ----------
+    base_opt : str
+        optimizer name
+
+    Returns
+    -------
+    module
+        optimizer class module.
+    """
+    base_opt_module = None
+    if base_opt in list(optim.__dict__.keys()):
+        base_opt_module = getattr(optim, base_opt)
+    elif base_opt in custom_opt:
+        base_opt_module = custom_opt[base_opt]
+    return base_opt_module
+    
 
 def get_loss(loss):
     if isinstance(loss, str):
