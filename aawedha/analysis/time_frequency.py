- Fixed float points precision issue in spectral power visualization  On branch dev  Changes to be committed: 	modified:   aawedha/analysis/time_frequency.py 	modified:   aawedha/visualization/signal.py

aawedha/analysis/time_frequency.py
@@ -53,7 +53,8 @@ def spectral_power(data, subject=0, channel='POz'):
 
     nyquist = data.fs / 2
     # frequencies = np.linspace(0, nyquist, np.floor(samples/2).astype(int)+1)
-    frequencies = np.arange(0, nyquist, data.fs/samples)
+    # frequencies = np.arange(0, nyquist, data.fs/samples)
+    frequencies = np.round(np.arange(0, nyquist, data.fs/samples), 14)
 
     # trials_per_class = np.round(trials / ev_count).astype(int)
     # power = np.zeros((len(frequencies), trials_per_class))

aawedha/visualization/signal.py
@@ -14,7 +14,7 @@ def harmonics_idx(freqs, event, h):
         ff = np.zeros((3))
     else:
         event = float(event)
-        ff = np.array([event*ev for ev in range(1, h+1)])
+        ff = np.array([np.round(event*ev, 14) for ev in range(1, h+1)])
     f_idx = np.logical_or.reduce([freqs == f for f in ff.tolist()])
     return f_idx
 