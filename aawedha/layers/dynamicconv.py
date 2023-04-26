f289fe95c2f2b325a20e9286c777b360c069024a
okba bekhelifi <okba.bekhelifi@gmail.com>
2023-04-26 11:24:54 +0200

    - Added DynCCNN model  On branch dev  Changes to be committed: 	new file:   aawedha/layers/dynamicconv.py 	new file:   aawedha/models/pytorch/dynccnn.py

---
+++ aawedha/layers/dynamicconv.py
@@ -0,0 +1,108 @@
+# Adadpted from: https://github.com/xmu-xiaoma666/External-Attention-pytorch/blob/master/model/conv/DynamicConv.py
+# Implements: Chen, Y., Dai, X., Liu, M., Chen, D., Yuan, L., &#38; Liu, Z. (2020). Dynamic convolution: 
+# Attention over convolution kernels. Proceedings of the IEEE Computer Society Conference on 
+# Computer Vision and Pattern Recognition, 11027–11036. https://doi.org/10.1109/CVPR42600.2020.01104
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torch.nn.modules.utils import _pair
+
+class Attention(nn.Module):
+    def __init__(self, in_planes, ratio, K, temprature=30,init_weight=True):
+        super().__init__()
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        self.temprature = temprature
+        assert in_planes>=ratio
+        hidden_planes = in_planes//ratio
+        self.net = nn.Sequential(
+            nn.Conv2d(in_planes, hidden_planes, kernel_size=1, bias=False),
+            nn.ReLU(),
+            nn.Conv2d(hidden_planes, K, kernel_size=1, bias=False)
+        )
+
+        if(init_weight):
+            self._initialize_weights()
+
+    def update_temprature(self):
+        if(self.temprature>1):
+            self.temprature-=1
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            if isinstance(m ,nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self,x):
+        att = self.avgpool(x) #bs, dim, 1, 1
+        att = self.net(att).view(x.shape[0], -1) #bs, K
+        return F.softmax(att / self.temprature, -1)
+
+class DynamicConv(nn.Module):
+    def __init__(self, in_planes, out_planes, kernel_size, stride, padding=0,
+                 dilation=1, grounps=1, bias=True ,K=4,
+                 temprature=30, ratio=4, init_weight=True):
+        super().__init__()
+        self.in_planes = in_planes
+        self.out_planes = out_planes
+        self.kernel_size =_pair(kernel_size)
+        self.stride = stride
+        self.padding = padding
+        self.dilation = dilation
+        self.groups = grounps
+        self.bias = bias
+        self.K = K
+        self.init_weight = init_weight
+        self.attention = Attention(in_planes=in_planes, ratio=ratio, K=K, temprature=temprature,
+                                 init_weight=init_weight)
+        
+        ks1, ks2 = self.kernel_size[0], self.kernel_size[1]
+
+        self.weight = nn.Parameter(torch.randn(K, out_planes, in_planes//grounps, ks1, ks2), requires_grad=True)
+        if(bias):
+            self.bias = nn.Parameter(torch.randn(K, out_planes), requires_grad=True)
+        else:
+            self.bias=None
+        
+        if(self.init_weight):
+            self._initialize_weights()
+
+        #TODO 初始化 (initialization)
+    def _initialize_weights(self):
+        for i in range(self.K):
+            nn.init.kaiming_uniform_(self.weight[i])
+
+    def forward(self,x):
+        bs, in_planels, h, w = x.shape
+        softmax_att = self.attention(x) #bs, K
+        x = x.view(1, -1, h, w)
+        weight = self.weight.view(self.K, -1) #K, -1
+        ks1, ks2 = self.kernel_size[0], self.kernel_size[1]
+        aggregate_weight = torch.mm(softmax_att, weight).view(bs*self.out_planes, 
+                                                              self.in_planes//self.groups,
+                                                              ks1, ks2) #bs*out_p, in_p, k, k
+
+        if(self.bias is not None):
+            bias = self.bias.view(self.K, -1) #K,out_p
+            aggregate_bias = torch.mm(softmax_att, bias).view(-1) #bs, out_p
+            output=F.conv2d(x, weight=aggregate_weight, bias=aggregate_bias, 
+                            stride=self.stride, padding=self.padding, 
+                            groups=self.groups*bs,
+                            dilation=self.dilation)
+        else:
+            output=F.conv2d(x, weight=aggregate_weight,
+                            bias=None, stride=self.stride,
+                            padding=self.padding,
+                            groups=self.groups*bs,
+                            dilation=self.dilation)
+
+        _, out, h1, w1 = output.shape
+        # output=output.view(bs,self.out_planes,h,w)
+        output=output.view(bs, self.out_planes, h1, w1)
+        return output
+
+    
---
+++ aawedha/models/pytorch/dynccnn.py
@@ -0,0 +1,77 @@
+from aawedha.models.pytorch.torchdata import reshape_input
+from aawedha.models.utils_models import is_a_loss
+from aawedha.layers.condconv import CondConv
+from aawedha.models.pytorch.eegtcnetpth import TemporalConvNet
+from torchlayers.regularization import L2
+from torch.nn.utils.parametrizations import spectral_norm
+from torch import flatten
+from torch import nn
+import torch.nn.functional as F
+import torch
+
+
+class DynCCNN(nn.Module):
+
+    def __init__(self, nb_classes=4, Chans=8,  dropout_rate=0.25, kernLength=10,
+                 fs=512, resolution=0.293, l2=0.0001, frq_band=[7, 70], 
+                 K=4, ratio=4, temperature=30, name='DynCCNN'):
+        super().__init__()    
+        self.name = name
+        self.fs = fs
+        self.resolution = resolution
+        self.nfft  = round(fs / resolution)
+        self.fft_start = int(round(frq_band[0] / self.resolution)) 
+        self.fft_end   = int(round(frq_band[1] / self.resolution)) + 1
+        dilation = (1, 3) 
+        samples = (self.fft_end - self.fft_start) * 2        
+        out_features = samples - dilation[1]*(kernLength-1) 
+        filters = 2*Chans
+
+        self.conv1 = DynamicConv(1, filters, (Chans, 1), stride=1, padding="valid",
+                                    grounps=1, bias=False ,K=K,
+                                    temprature=30, ratio=1, init_weight=True)
+                                   
+        self.ln1   = nn.BatchNorm2d(filters) # nn.LayerNorm([filters, 1, samples])
+        self.drop1 = nn.Dropout(dropout_rate)
+        
+        self.conv2 = DynamicConv(filters, filters, (1, 10), stride=1, padding="valid",
+                                    dilation=dilation, grounps=1, bias=False ,K=K,
+                                    temprature=30, ratio=4, init_weight=True)
+                                   
+        self.ln2   = nn.BatchNorm2d(filters) # nn.LayerNorm([filters, 1, out_features])
+        self.drop2 = nn.Dropout(dropout_rate)
+        self.fc    = L2(nn.Linear(filters*out_features, nb_classes), weight_decay=l2)
+
+        self.init_weights()
+            
+    def init_weights(self):
+        for module in self.modules():
+            cls_name = module.__class__.__name__
+            if not is_a_loss(module) and not "ModuleDict" in cls_name:  
+                if hasattr(module, 'weight'):
+                    cls_name = module.__class__.__name__            
+                    if not("BatchNorm" in cls_name or "LayerNorm" in cls_name):
+                        nn.init.normal_(module.weight, mean=0.0, std=0.01)
+                    else:
+                        nn.init.constant_(module.weight, 1)
+                if hasattr(module, "bias"):
+                    if module.bias is not None:
+                        nn.init.constant_(module.bias, 0)    
+    
+    def forward(self, x):
+        x = reshape_input(x)
+        x = self.transform(x)
+        x = self.drop1(F.relu(self.ln1(self.conv1(x))))
+        x = self.drop2(F.relu(self.ln2(self.conv2(x))))    
+        x = flatten(x, 1)
+        x = self.fc(x)
+        return x
+
+    def transform(self, x):
+        with torch.no_grad():
+            samples = x.shape[-1]
+            x = torch.fft.rfft2(x, s=self.nfft, dim=-1) / samples
+            real = x.real[:,:,:, self.fft_start:self.fft_end]
+            imag = x.imag[:,:,:, self.fft_start:self.fft_end]
+            x = torch.cat((real, imag), axis=-1)
+        return x
