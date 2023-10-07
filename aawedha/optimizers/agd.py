6b6fe1cd522fb093bc5964882f71dc9132c4be15
okba <okba.bekhelifi@gmail.com>
2023-10-07 16:52:40 +0100

    - Added optimizers 	- Aida 	- Lion 	- AGD 	- Prodigy  On branch dev  Changes to be committed: 	new file:   aawedha/optimizers/agd.py 	new file:   aawedha/optimizers/aida.py

---
+++ aawedha/optimizers/agd.py
@@ -0,0 +1,56 @@
+# taken from official code repo : https://github.com/jxbz/agd/
+# Implements the Automatic Gradient Descent (AGD) optimizer published in:
+# Bernstein J, Mingard C, Huang K, Azizan N, Yue Y. Automatic Gradient Descent: 
+# Deep Learning without Hyperparameters. 2023; 
+# Available from: http://arxiv.org/abs/2304.05187
+import math
+import torch
+
+from torch.optim.optimizer import Optimizer
+from torch.nn.init         import orthogonal_
+
+def singular_value(p):
+    sv = math.sqrt(p.shape[0] / p.shape[1])
+    if p.dim() == 4:
+        sv /= math.sqrt(p.shape[2] * p.shape[3])
+    return sv
+
+class AGD(Optimizer):
+
+    def __init__(self, net, gain=1.0):
+
+        self.net = net
+        self.depth = len(list(net.parameters()))
+        self.gain = gain
+
+        for p in self.net.parameters():
+            if p.dim() == 1: raise Exception("Biases are not supported.")
+
+        super().__init__(net.parameters(), defaults=dict())
+
+    @torch.no_grad()
+    def init_weights(self):
+
+        for p in self.net.parameters():
+            if p.dim() == 2: orthogonal_(p)
+            if p.dim() == 4:
+                for kx in range(p.shape[2]):
+                    for ky in range(p.shape[3]):
+                        orthogonal_(p[:,:,kx,ky])
+            p *= singular_value(p)
+
+    @torch.no_grad()
+    def step(self):
+
+        G = 0
+        for p in self.net.parameters():
+            G += singular_value(p) * p.grad.norm(dim=(0,1)).sum()
+        G /= self.depth
+
+        log = math.log(0.5 * (1 + math.sqrt(1 + 4*G)))
+
+        for p in self.net.parameters():
+            factor = singular_value(p) / p.grad.norm(dim=(0,1), keepdim=True)
+            p -= self.gain * log / self.depth * torch.nan_to_num(factor) * p.grad
+
+        return log
